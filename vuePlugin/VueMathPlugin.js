aa3590280d5b78050db63215d94f5829f2852fba
1456121347 <1456121347@qq.com>
2020-04-13 22:38:20 +0800

    笔记提交完毕

--- vuePlugin/VueMathPlugin.js
+++ vuePlugin/VueMathPlugin.js
@@ -7,5 +7,18 @@ export default {
         Vue.directive('sqrt',function(el,binding){
             el.innerHTML = Math.sqrt(binding.value)
         });
+        // sin
+        Vue.directive('sin',function(el,binding){
+            el.innerHTML = Math.sin(binding.value)
+        });
+        // cos
+        Vue.directive('cos',function(el,binding){
+            el.innerHTML = Math.cos(binding.value)
+        });
+        // tan
+        Vue.directive('tan',function(el,binding){
+            el.innerHTML = Math.tan(binding.value)
+        });
     }
 }
+
