02f307fb693130ee40d4b0703b6ec2cd37b9aa22
1456121347 <1456121347@qq.com>
2020-04-15 17:14:16 +0800

    simple提交

--- simple/src/Layout.vue
+++ simple/src/Layout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="body">
     <div class="app-head">
       <div class="app-head-inner">
         <img src="./assets/logo.png" alt="">
@@ -18,7 +18,7 @@
       <router-view></router-view>
     </div>
     <div class="app-footer">
-      <p>&copy; 1903C — 2020</p>
+      <p>&copy; 1903C - 2020</p>
     </div>
   </div>
 </template>
@@ -29,47 +29,13 @@ export default {
 </script>
 
 <style>
-/* reset样式 */
-html,body,div,span,applet,object,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video {
-	margin:0;
-	padding:0;
-	border:0;
-	font-size:100%;
-	font:inherit;
-	font-weight:normal;
-	vertical-align:baseline;
-}
-ol,ul,li {
-	list-style:none;
-}
-blockquote,q {
-	quotes:none;
-}
-blockquote:before,blockquote:after,q:before,q:after {
-	content:'';
-	content:none;
-}
-table {
-	border-collapse:collapse;
-	border-spacing:0;
-}
-th,td {
-	vertical-align:middle;
-}
-/* custom */
-a {
-	outline:none;
-	color:#16418a;
-	text-decoration:none;
-	-webkit-backface-visibility:hidden;
-}
-a:focus {
-	outline:none;
-}
-input:focus,select:focus,textarea:focus {
-	outline:-webkit-focus-ring-color auto 0;
-}
+
 /* layout样式 */
+.body{
+  background: #f0f2f5;
+  font-size: 15px;
+  color: #444;
+}
 .app-head {
   background: #363636;
   color: #b2b2b2;
@@ -107,5 +73,45 @@ input:focus,select:focus,textarea:focus {
 .app-content{
   width: 1200px;
   margin: 0 auto;
+}
+/* reset样式 */
+html,body,div,span,applet,object,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video {
+	margin:0;
+	padding:0;
+	border:0;
+	font-size:100%;
+	font:inherit;
+	font-weight:normal;
+	vertical-align:baseline;
+}
+ol,ul,li {
+	list-style:none;
+}
+blockquote,q {
+	quotes:none;
+}
+blockquote:before,blockquote:after,q:before,q:after {
+	content:'';
+	content:none;
+}
+table {
+	border-collapse:collapse;
+	border-spacing:0;
+}
+th,td {
+	vertical-align:middle;
+}
+/* custom */
+a {
+	outline:none;
+	color:#16418a;
+	text-decoration:none;
+	/* -webkit-backface-visibility:hidden; */
+}
+a:focus {
+	outline:none;
+}
+input:focus,select:focus,textarea:focus {
+	outline:-webkit-focus-ring-color auto 0;
 }
 </style>
--- simple/src/pages/index.vue
+++ simple/src/pages/index.vue
@@ -1,15 +1,161 @@
 <template>
-    <div>
-        index首页
+  <div class="index-wrapper">
+    <div class="index-left">
+      <!-- 全部产品 -->
+      <div class="index-left-block">
+        <h2>全部产品</h2>
+        <template v-for="product in productList">
+          <h3>{{ product.title }}</h3>
+          <ul>
+            <li v-for="item in product.list">
+              <a v-bind:href="item.url">{{ item.title}}</a>
+            </li>
+          </ul> 
+          <div v-if="product.title == 'PC产品'" class="hr"></div>
+        </template>
+      </div>
+      <!-- 最新消息 -->
+      <div class="index-left-block lastest-news">
+        <h2>最新消息</h2>
+        <ul>
+          <li>ddd</li>
+        </ul>
+      </div>
     </div>
+    <div class="index-right">
+      <div style="margin:0 auto;width:700px;height:300px;background:red;">将来用组件代替</div>
+      <div class="index-boader-list">
+        <div class="index-boader-item">
+          <h2>第一个标题</h2>
+          <p>第一个描述</p>
+          <div class="index-boader-button">立即够买</div>
+        </div>
+        <div class="index-boader-item">
+          <h2>第二个标题</h2>
+          <p>第二个描述</p>
+          <div class="index-boader-button">立即够买</div>
+        </div>
+        <div class="index-boader-item">
+          <h2>第三个标题</h2>
+          <p>第三个描述</p>
+          <div class="index-boader-button">立即够买</div>
+        </div>
+        <div class="index-boader-item">
+          <h2>第四个标题</h2>
+          <p>第四个描述</p>
+          <div class="index-boader-button">立即够买</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      productList: {
+        pc: {
+          title: "PC产品",
+          list: [
+            {
+              title: "数据统计",
+              url: "http://starcraft.com"
+            },
+            {
+              title: "数据预测",
+              url: "http://warcraft.com"
+            },
+            {
+              title: "流量分析",
+              url: "http://overwatch.com",
+              hot: true
+            },
+            {
+              title: "广告发布",
+              url: "http://hearstone.com"
+            }
+          ]
+        },
+        app: {
+          title: "手机应用类",
+          list: [
+            {
+              title: "91助手",
+              url: "http://weixin.com"
+            },
+            {
+              title: "产品助手",
+              url: "http://weixin.com"
+            },
+            {
+              title: "智能地图",
+              url: "http://maps.com"
+            },
+            {
+              title: "语音助手",
+              url: "http://phone.com"
+            }
+          ]
+        }
+      }
+    };
+  }
+};
 </script>
 
-<style>
-
+<style scoped>
+.index-wrapper {
+  display: flex;
+  /* background: #e3e4e8;    */
+}
+.index-left {
+  width: 30%;
+}
+.index-right {
+  width: 70%;
+}
+.index-left-block {
+  margin: 15px;
+  background: white;
+  border-radius: 5px;
+  box-shadow: 0 0 1px #dddddd;
+}
+.index-left-block .hr {
+  border-bottom: 1px solid black;
+  margin: 20px 0;
+}
+.index-left-block h2 {
+  background: #4fc08d;
+  color: #ffffff;
+  padding: 10px 15px;
+  margin-bottom: 20px;
+}
+.index-left-block h3 {
+  color: #222;
+  font-weight: bolder;
+  padding: 0 15px 5px 15px;
+}
+.index-left-block ul {
+  padding: 10px 15px;
+}
+.index-left-block li {
+  padding: 5px;
+}
+.index-boader-list {
+  margin-top: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.index-boader-item {
+  width: 30%;
+  height: 125px;
+  padding-left: 120px;
+  background: #ffffff;
+  margin-right: 1%;
+  box-shadow: 0 0 1px #ddd;
+  margin-bottom: 20px;
+  border-radius: 10px 10px 10px 10px;
+}
 </style>
